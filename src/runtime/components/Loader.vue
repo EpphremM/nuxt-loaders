48709d43522cee3d6caca3561dc7c9eec1a602c2
HaileabT <haileabtesfaye11@gmail.com>
2025-12-08 15:59:04 -0500

    init: initialized project with directory structure

---
+++ src/runtime/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Hi</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue";
+
+const log = () => {
+  console.log("this is the loader");
+};
+
+onMounted(log);
+</script>
